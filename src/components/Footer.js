093bbced27333a40d4ba26e6de4d7aa97604b9c2
Bagar Hynek <bagar@bagarmac.domain.local>
2022-05-10 09:44:37 +0200

    my new footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ export function Footer() {
   return (
     <Container as="footer">
       <hr />
+      <p>This is my new Footer lets check this out</p>
       <p>&copy; {new Date().getFullYear()} &middot; CN Group CZ a.s.</p>
     </Container>
   );
