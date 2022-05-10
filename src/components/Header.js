f2bdc09ae10ef4d425062fa624070376f1c5ece8
Bagar Hynek <bagar@bagarmac.domain.local>
2022-05-10 09:57:25 +0200

    different Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export function Header() {
   return (
     <Navbar color="dark" dark>
       <Container>
+        <p>this is how i want my header to be</p>
         <NavbarBrand href="/">Cookbook</NavbarBrand>
       </Container>
     </Navbar>
