5a720995dc03f02ad18e310b291980f2a7d1ba48
Bagar Hynek <bagar@bagarmac.domain.local>
2022-05-10 09:54:51 +0200

    my new Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export function Header() {
   return (
     <Navbar color="dark" dark>
       <Container>
+        <h1>Welcome in the new feature header</h1>
         <NavbarBrand href="/">Cookbook</NavbarBrand>
       </Container>
     </Navbar>
